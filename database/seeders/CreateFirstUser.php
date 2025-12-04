df2efc1467b8886cef93aa8a08ce0f3e0af78182
IdoRefaelSiregar24 <ido24si@mahasiswa.pcr.ac.id>
2025-12-04 08:16:19 +0700

    Update file seeders create first user

--- database/seeders/CreateFirstUser.php
+++ database/seeders/CreateFirstUser.php
@@ -15,17 +15,38 @@ class CreateFirstUser extends Seeder
     public function run(): void
     {
         User::create([
+            'name' => 'Surveyor Binda Desa',
+            'email' => 'Surveyor@gmail.com',
+            'password' => Hash::make('Asdfg123'),
+            'role' => 'Surveyor',
+        ]);
+
+        User::create([
+            'name' => 'Manajer Lapangan',
+            'email' => 'manajerlapangan@gmail.com',
+            'password' => Hash::make('Asdfg123'),
+            'role' => 'Manajer Lapangan',
+        ]);
+
+        User::create([
+            'name' => 'User Binda Desa',
+            'email' => 'userbinadesa@gmail.com',
+            'password' => Hash::make('Asdfg123'),
+            'role' => 'User',
+        ]);
+
+        User::create([
             'name' => 'Admin Proyek',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('Asdfg123'),
-            'role' => 'admin',
+            'role' => 'Admin Proyek',
         ]);
 
         User::create([
             'name' => 'Ido Refael Siregar',
             'email' => 'ido24si@mahasiswa.pcr.ac.id',
             'password' => Hash::make('Asdfg123'),
-            'role' => 'user',
+            'role' => 'Super Admin',
         ]);
     }
 }
