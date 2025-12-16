8a100c6fd6241d4d3cef5e040f31ab9baee06043
IdoRefaelSiregar24 <ido24si@mahasiswa.pcr.ac.id>
2025-12-16 15:29:19 +0700

    Update Migration

--- database/migrations/2025_12_16_024704_create_table_lokasi_proyek.php
+++ database/migrations/2025_12_16_024704_create_table_lokasi_proyek.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('table_lokasi_proyek', function (Blueprint $table) {
-            $table->id();
+        Schema::create('lokasi_proyek', function (Blueprint $table) {
+            $table->id('lokasi_id');
+            $table->unsignedBigInteger('proyek_id');
+            $table->decimal('lat', 10, 7);
+            $table->decimal('lng', 10, 7);
+            $table->json('geojson')->nullable();
             $table->timestamps();
+
+           $table->foreign('proyek_id')
+                  ->references('proyek_id')
+                  ->on('proyek')
+                  ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('table_lokasi_proyek');
+        Schema::dropIfExists('lokasi_proyek');
     }
 };
--- database/migrations/2025_12_16_024743_create_table_kontraktor.php
+++ database/migrations/2025_12_16_024743_create_table_kontraktor.php
@@ -4,24 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('table_kontraktor', function (Blueprint $table) {
-            $table->id();
+        Schema::create('kontraktor', function (Blueprint $table) {
+            $table->id('kontraktor_id');
+            $table->unsignedBigInteger('proyek_id');
+            $table->string('nama');
+            $table->string('penanggung_jawab');
+            $table->string('kontak');
+            $table->text('alamat');
             $table->timestamps();
+
+            $table->foreign('proyek_id')
+                  ->references('proyek_id')
+                  ->on('proyek')
+                  ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('table_kontraktor');
+        Schema::dropIfExists('kontraktor');
     }
 };
